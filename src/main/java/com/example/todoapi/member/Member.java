3397ff72da9196b43dc5cb9fbd9aa71d8d4fda3f
mirupio <162397816+mirupio@users.noreply.github.com>
2024-10-30 15:54:18 +0900

    hw4

--- src/main/java/com/example/todoapi/member/Member.java
+++ src/main/java/com/example/todoapi/member/Member.java
@@ -1,4 +1,41 @@
 package com.example.todoapi.member;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="member_id")
+    private Long id;
+
+    @Column(name = "member_login_id", length = 20, nullable = false)
+    private String loginId;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    // Getter for id
+    public Long getId() {
+        return id;
+    }
+
+    // Getter for loginId
+    public String getLoginId() {
+        return loginId;
+    }
+
+    // Getter for password
+    public String getPassword() {
+        return password;
+    }
+    // 필드 수정 메서드
+    public void updateLoginId(String newLoginId) {
+        this.loginId = newLoginId;
+    }
+
+    public void updatePassword(String newPassword) {
+        this.password = newPassword;
+    }
+
 }
--- src/main/java/com/example/todoapi/todo/Todo.java
+++ src/main/java/com/example/todoapi/todo/Todo.java
@@ -1,4 +1,37 @@
 package com.example.todoapi.todo;
 
+import com.example.todoapi.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@Getter
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "todo_id")
+    private Long id;
+
+    @Column(name = "todo_content",columnDefinition = "varchar(200)")
+    private String content;
+
+    @Column(name = "todo_is_checked",columnDefinition = "tinyint(1)")
+    private boolean isChecked;
+
+    @JoinColumn(name="member_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    public Todo(String content, boolean isChecked, Member member) {
+        this.content = content;
+        this.isChecked = isChecked;
+        this.member = member;
+    }
+
+    public void updateContent(String newContent){
+        this.content = newContent;
+    }
 }
